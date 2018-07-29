3d83ff5ca84adcfc84684c2a8e9917bbe66dab77
Suleiman Ali <suleiman.ali@catenate.com>
2018-07-29 10:32:42 +0200

    set up conda FLASK env and vs code workspace

---
+++ blockchain.py
@@ -0,0 +1,25 @@
+class Blockchain (object):
+    def __init__(self):
+        self.chain = []
+        self.current_transaction = []
+
+    def new_block(self):
+        #Creates a new block and adds it to the chain
+        pass
+    
+    def new_transaction(self):
+        #Adds a new transaction to the list of transactions
+        pass
+    
+    @staticmethod
+
+    def hash(block):
+        #hashes a block
+        pass
+
+    @property
+    def last_block(self):
+        #returns the last Block in the Chain
+        pass
+
+        
